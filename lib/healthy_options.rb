49aa20f01c20fdc5f84f20681eba14754476e7ae
Rick Hull <rick.hull@gmail.com>
2017-01-28 15:39:05 -0800

    tests are passing!

---
+++ lib/healthy_options.rb
@@ -0,0 +1,189 @@
+module HealthyOptions
+  RGX = {
+    long: %r{
+    \A       # starts with
+    --       # double dash
+    (?'flag' # named group
+      (?:    # non-capturing group
+        \w+  # word chars
+        -?   # possible dash
+      )*     # some number of word-word- (possibly none)
+      \w+    # one last word (possibly first as well)
+    )        # end flag group
+    }x,      # end :long regex
+    short: %r{
+    \A       # starts with
+    -        # single dash
+    (?'flag' # named group
+      \w     # word char
+    )        # end flag group
+    }x,      # end :short regex
+  }
+
+  # more than temporary, for now
+  # reverse index for the FLAGS structure
+  INDEX = {
+    long: {},
+    short: {},
+  }
+
+  # sample structure, just temporary for prototyping
+  FLAGS = {
+    foo: {
+      long: 'foo',
+      short: 'f',
+      value: true,
+    },
+    bar: {
+      long: 'bar',
+      short: 'b',
+      value: false,
+    },
+  }
+
+  # temporary, this should happen after requiretime
+  FLAGS.each { |sym, hsh|
+    INDEX[:long][hsh[:long]] = sym if hsh[:long]
+    INDEX[:short][hsh[:short]] = sym if hsh[:short]
+  }
+
+  def self.parse(args, opts = {})
+    return [args, opts] if args.empty?
+    return [args, opts] unless self.flag?(args.first)
+
+    res, flag, value = self.check_flag(args.first)
+    raise("unrecognized flag: #{args.first}") if res == :no_flag
+    raise("flag expected for #{res}") unless flag
+
+    sym = INDEX[:long][flag] || INDEX[:short][flag]
+    raise "unrecognized flag: #{flag}" unless sym
+    args.shift
+
+    case res
+    when :flag_has_val
+      raise("value expected") unless value
+      opts[sym] = value
+    when :flag_need_val
+      value = args.shift
+      raise "flag #{flag} needs a value; none provided" unless value
+      raise "flag #{flag} needs a value; got #{value}" if self.flag?(value)
+      opts[sym] = value
+    when :flag_no_val
+      opts[sym] = true
+    when :flag_no_val_more
+      opts[sym] = true
+      raise("more exected for #{flag} parsed as #{res}") unless value
+      # look for smashed flags
+      opts = opts.merge(self.parse_smashed(value))
+    else
+      raise "unknown result: #{res}"
+    end
+    self.parse(args, opts)
+  end
+
+  def self.parse_smashed(arg)
+    opts = {}
+    # preceding dash and flag have been removed
+    val = arg.dup
+    loop {
+      break if val.empty?
+      char = val.slice!(0, 1)
+      sym = INDEX[:short][char]
+      raise "unknown flag smashed in: #{char} in #{arg}" unless sym
+      spec = FLAGS.fetch(sym)
+      # TODO: error handling (punctuation, -p5 -5p, etc)
+      if spec[:value]
+        opts[sym] = val
+        break
+      else
+        opts[sym] = true
+      end
+    }
+    opts
+  end
+
+  def self.flag?(arg)
+    arg[0] == '-'
+  end
+
+  def self.check_flag(arg)
+    flag = nil
+    flag_type = nil
+    RGX.each { |ft, rgx|
+      if (m = rgx.match(arg))
+        flag = m['flag']
+        flag_type = ft
+        break
+      end
+    }
+    unless flag_type
+      # arg is not a flag
+      # do a sanity check for caller's sake
+      raise "arg should not be empty" if arg.nil? or arg.empty?
+      return [:no_flag]
+    end
+    sym = INDEX.dig(flag_type, flag)
+    return [:unknown_flag, flag] unless sym
+    spec = FLAGS.fetch(sym)
+    if spec[:value]
+      # either we have it here, or the rest of the arg is empty
+      if flag_type == :short
+        # anything the arg has must be the value
+        val = arg.dup
+        dashflag = val.slice!(0, 2)
+        raise("expected #{arg} to lead with -#{flag}") if dashflag != "-#{flag}"
+        return val.empty? ? [:flag_need_val, flag] : [:flag_has_val, flag, val]
+      else
+        val = arg.dup
+        dashes = val.slice!(0, 2)
+        raise("expected #{arg} arg to lead with --") if dashes != '--'
+        flagcheck = val.slice!(0, flag.length)
+        if flagcheck != flag
+          raise("expected #{arg} (#{flagcheck}) to match #{flag}")
+        end
+        if val.empty?
+          return [:flag_need_val, flag]
+        else
+          if val[0] == '='
+            val.slice!(0, 1)
+            if val.empty?
+              raise("a value is required after =: #{arg}")
+            else
+              return [:flag_has_val, flag, val]
+            end
+          else
+            raise("#{flag} requires a value but followed by #{val}")
+          end
+        end
+      end
+    else
+      if flag_type == :short
+        # next char must not be equals
+        val = arg.dup
+        dashflag = val.slice!(0, 2)
+        raise("expected #{arg} to lead with -#{flag}") if dashflag != "-#{flag}"
+        if val.empty?
+          return [:flag_no_val, flag]
+        elsif val[0] == '='
+          raise("#{flag} does not take a value: #{arg}")
+        else
+          return [:flag_no_val_more, flag, val]
+        end
+      else
+        # make sure this is the end of the arg
+        val = arg.dup
+        dashes = val.slice!(0, 2)
+        raise("expected #{arg} arg to lead with --") if dashes != '--'
+        flagcheck = val.slice!(0, flag.length)
+        if flagcheck != flag
+          raise("expected #{arg} (#{flagcheck}) to match #{flag}")
+        end
+        if val.empty?
+          return [:flag_no_val, flag]
+        else
+          raise("#{flag} does not take a value: #{arg}")
+        end
+      end
+    end
+  end
+end
---
+++ test/spec.rb
@@ -0,0 +1,143 @@
+require 'minitest/autorun'
+require 'healthy_options'
+
+describe HealthyOptions do
+  before do
+    @flags = {
+      foo: {
+        long: 'foo',
+        short: 'f',
+        value: true,
+      },
+      bar: {
+        long: 'bar',
+        short: 'b',
+        value: false,
+      },
+    }
+  end
+
+  describe "parse_args" do
+    describe "long options" do
+      describe "value options" do
+        # VALID
+        #   --foo bar
+        #   --foo=bar
+        valid = [['--foo', 'bar'],
+                 ['--foo=bar']]
+        # INVALID
+        #   --foo= bar (consider new feature)
+        #   --foo -bar
+        #   --foo <EOF>
+        invalid = [['--foo=', 'bar'],
+                   ['--foo', '-bar'],
+                   ['--foo'],
+                  ]
+
+        valid.each do |valid_args|
+          it "must parse #{valid_args}" do
+            args, opts = HealthyOptions.parse(valid_args)
+            args.must_be_instance_of(Array)
+            args.must_be_empty
+            opts.must_be_instance_of(Hash)
+            opts.wont_be_empty
+            opts[:foo].must_equal("bar")
+          end
+        end
+
+        invalid.each do |invalid_args|
+          it "must reject #{invalid_args}" do
+            proc { HealthyOptions.parse(invalid_args) }.must_raise RuntimeError
+          end
+        end
+      end
+
+      describe "flag options" do
+        # VALID
+        #   --bar
+        #   --bar-baz
+        #   --bar_baz
+        valid = [['--bar'],
+               #  ['--bar-baz'],  these are unrecognized
+               #  ['--bar_baz'],
+                ]
+
+        # INVALID
+        #   --bar=
+        #   -bar    (could be `-b ar`, or `-b -ar`, or `-b -a -r`)
+        #   --.bar
+        #   ---bar
+        #   --
+        invalid = [['--bar='],
+                   ['-bar'],
+                   ['--.bar'],
+                   ['---bar'],
+                   ['--'],
+                  ]
+
+        valid.each do |valid_args|
+          it "must parse #{valid_args}" do
+            args, opts = HealthyOptions.parse(valid_args)
+            args.must_be_instance_of(Array)
+            args.must_be_empty
+            opts.must_be_instance_of(Hash)
+            opts.wont_be_empty
+            opts[:bar].must_equal(true)
+          end
+        end
+
+        invalid.each do |invalid_args|
+          it "must reject #{invalid_args}" do
+            proc { HealthyOptions.parse(invalid_args) }.must_raise RuntimeError
+          end
+        end
+      end
+    end
+
+    describe "short options" do
+      describe "value options" do
+        # VALID (for p flag)
+        #   -p 5
+        #   -p=5
+        # SMASHED VALUE (valid)
+        #   -p5
+        # SMASHED FLAG (valid)
+        #   -op 5
+        #   -op=5
+        # SMASHED FLAG & VALUE
+        #   -op5
+      end
+
+      describe "smashed value options" do
+        # SMASHED VALUE (valid)
+        #   -p5
+        # SMASHED FLAG & VALUE
+        #   -op5
+      end
+
+      describe "flag options" do
+        # VALID
+        # -p
+        # INVALID
+        # -poo
+      end
+
+      describe "smashed flag options" do
+        # VALID (o=flag, p=flag, q=value)
+        # -op
+        # -pq 5
+        # -oq5
+        # -opq=5
+        # -opq 5
+      end
+    end
+
+    describe "mixed long and short options" do
+      describe "independent namespaces" do
+        # tempf:  --f     -F
+        # food:   --food  -f
+        # expect: No collision
+      end
+    end
+  end
+end
